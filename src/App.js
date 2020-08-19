9fea433f66513ce8a3bda738d419ba2427404a19
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-08-19 13:20:45 +0200

    auth routers are done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Private from "./pages/Private";
+import AuthProvider from "./lib/AuthProvider";
+import AnonRoute from "./components/AnonRoute";
+import PrivateRoute from "./components/PrivateRoute";
+
+class App extends Component {
+  render() {
+    return (
+      <AuthProvider>
+        <div className="container">
+          <Navbar />
+
+          <Switch>
+            <AnonRoute path="/signup" component={Signup} />
+            <AnonRoute path="/login" component={Login} />{" "}
+            <PrivateRoute path="/private" component={Private} />
+          </Switch>
+        </div>
+      </AuthProvider>
+    );
+  }
+}
+
+export default App;
