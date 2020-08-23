8ab8e8a8195d1aa8c277adce392039e4902320c6
mmazzariello <mfmazzariello@gmail.com>
2020-08-23 11:41:38 +0000

    New verion

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { Component } from "react";
-import { Switch } from "react-router-dom";
-// import { BrowserRouter as Router } from "react-router-dom"; // <== !!!
-import AuthProvider from "./lib/AuthProvider";
+import React from 'react';
+import { Router } from './router'
+import AuthProvider from './utils/AuthProvider'
+import './App.css';
 
-import Botones from "./components/Botones";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Private from "./pages/Private";
-import AnonRoute from "./components/AnonRoute";
-import PrivateRoute from "./components/PrivateRoute";
-import TaskPage from "./components/tasks/TaskPage";
-import Profile from "./components/profile/Profile";
-// import Home from "./pages/Home";
-// import { Helmet } from "react-helmet";
-// import { BrowserRouter as Router } from "react-router-dom"; // <== !!!
-import "bootstrap/dist/css/bootstrap.min.css";
-import Group from "./components/group/Group";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <AuthProvider>
-          <Botones />
-
-          <Switch>
-            <AnonRoute path="/signup" component={Signup} />
-            <AnonRoute path="/login" component={Login} />
-            <AnonRoute path="/group" component={Group} />
-            <PrivateRoute path="/private" component={Private} />
-            <PrivateRoute exact path="/tasks" component={TaskPage} />
-            <PrivateRoute exact path="/profile" component={Profile} />
-          </Switch>
-        </AuthProvider>
-      </div>
-    );
-  }
+function App () {
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 }
 
 export default App;
