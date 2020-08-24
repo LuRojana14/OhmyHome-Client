d5424be18204f3c9e64bef4b84d7685828cde7d1
Lurojana14 <lourdes_rojana@hotmail.com>
2020-08-24 19:59:02 +0200

    home styles

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-import logo from "./../../assets/logoynombre.png"
+import { Link } from "react-router-dom";
+import logo from "./../../assets/locoppal.png"
 import "./home.css"
 
 
@@ -11,8 +12,15 @@ class HomePage extends Component {
         <Helmet>
         <body className="home"/>
         </Helmet>
+        <div className="contenedor-main">
         <div className="contenedor-foto">
-        <img className="fotoCasa" src={logo} alt="" /> 
+            <img className="fotoCasa" src={logo} alt="" />
+        </div>
+            <div className="titulo"><h2>ohMyHome</h2></div> 
+        </div>
+        <div className="contenedor-botonesHome">
+        <Link to="/signup"><button className="button-home">New Home</button></Link>
+        <Link to="/login"><button className="button-home">Create Home</button></Link>
         </div>
         </div>
       );
